61cbc7cf7c50c3ffa34a20c88a266e36038ad412
webPeng97 <2571879534@qq.com>
2019-07-23 16:19:41 +0800

    updata home

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,23 +1,34 @@
-import { SearchBar, Carousel  } from 'antd-mobile'
+import { SearchBar, Carousel, Tabs   } from 'antd-mobile'
 import React, { Component, Fragment  } from 'react'
+import { getRoleList } from '../../api'
+
 import Img01 from "../../assets/images/1.png"
 import Img02 from "../../assets/images/2.png"
 import Img03 from "../../assets/images/3.png"
 import Img04 from "../../assets/images/4.png"
 
+const tabs = [
+  { title: <span>最新</span> },
+  { title: <span>专题</span> },
+  { title: <span>寻味</span> },
+  { title: <span>知识</span> }
+]
+
 class Home extends Component {
   state = {
     data: [Img01, Img02, Img03, Img04],
-    imgHeight: 176
+    imgHeight: 176,
+    asyncList: []
   }
 
-  // componentDidMount () {
-  //   setTimeout(() => {
-  //     this.setState({
-  //       data: ['1', '2', '3', '4'],
-  //     })
-  //   }, 100)
-  // }
+  componentDidMount () {
+    getRoleList().then(res => {
+      console.log(res.data.data)
+      this.setState({
+        asyncList: res.data.data
+      })
+    })
+  }
 
   render () {
     return (
@@ -56,6 +67,32 @@ class Home extends Component {
             </a>
           ))}
         </Carousel>
+        <Tabs tabs={tabs}
+          initialPage={1}
+          onChange={(tab, index) => { console.log('onChange', index, tab); }}
+          onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+        >
+          <div className="latest">
+            <div className="title">从开始你就赢了</div>
+            <div className="text">最新</div>
+          </div>
+          <div className="monographic">
+            专题
+            {
+              this.state.asyncList.map(item => {
+                return (
+                  <p key={item.id}>{ item.name }</p>
+                )
+              })
+            }
+          </div>
+          <div className="ruminate">
+            寻味
+          </div>
+          <div className="knowledge">
+            知识
+          </div>
+        </Tabs>
       </Fragment>
     )
   }
